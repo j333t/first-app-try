cd096092becec19a027f593403988cf7dbdb896b
Jeet Shah <61413406+j333t@users.noreply.github.com>
2024-10-10 18:09:12 +0530

    added slick css to schedular/app

--- schedular/src/App.js
+++ schedular/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DeliveryList from './components/DeliveryList';
 import DeliveryDetail from './components/DeliveryDetail';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
