190c6d90c9f1f38afa560ea28ab78ac81a68970e
Jeet Shah <61413406+j333t@users.noreply.github.com>
2024-10-11 13:27:04 +0530

    updated root in vite config

--- schedular/vite.config.js
+++ schedular/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: './schedular',
+  root: '.',
   build: {
     outDir: 'dist',
   },
