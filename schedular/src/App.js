2bd68e01cc55a11d6fcf51bc3f070a898f9a7c88
Scratchlearn <109966255+Scratchlearn@users.noreply.github.com>
2024-11-06 12:49:50 +0530

    Update App.js

--- schedular/src/App.js
+++ schedular/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google'; // Import the provider
 import DeliveryList from './components/DeliveryList';
 import DeliveryDetail from './components/DeliveryDetail';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<DeliveryList />} />
-        <Route path="/delivery/*" element={<DeliveryDetail />} />
+  //const clientId = "YOUR_CLIENT_ID"; // Replace with your actual client ID
 
-      </Routes>
-    </Router>
+  return (
+    <GoogleOAuthProvider clientId='47439091557-2kdm5q9pi6lm0d2n3pvtvb6vr9p69h4h.apps.googleusercontent.com'> {/* Wrap your application with the provider */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<DeliveryList />} />
+          <Route path="/delivery/*" element={<DeliveryDetail />} />
+        </Routes>
+      </Router>
+    </GoogleOAuthProvider>
   );
 }
 
